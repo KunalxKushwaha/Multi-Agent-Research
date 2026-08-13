f2c438b11f49f2bb18714fad65f50f77055769fe
KunalxKushwaha <kushwahakunal733@gmail.com>
2026-08-13 22:30:34 +0530

    Starting the new project

---
+++ agents.py
@@ -0,0 +1,10 @@
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#model setup 
+llm = ChatOpenAI(model = "gpt-4o-mini",temperature=0)
